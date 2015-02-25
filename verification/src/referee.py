44f2354fedbc51d0e48bff1c82ff9c7406e292d2
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-02-25 11:56:36 +0300

    ref/tests

---
+++ verification/src/referee.py
@@ -0,0 +1,22 @@
+from checkio_referee import RefereeBase
+from checkio_referee.covercode import py_unwrap_args
+
+import settings
+import settings_env
+from tests import TESTS
+
+cover = """def cover(f, data):
+    return f(d[0], str(d[1]))
+"""
+
+
+class Referee(RefereeBase):
+    TESTS = TESTS
+    EXECUTABLE_PATH = settings.EXECUTABLE_PATH
+    CURRENT_ENV = settings_env.CURRENT_ENV
+    FUNCTION_NAME = "mark_patterns"
+    ENV_COVERCODE = {
+        "python_2": py_unwrap_args,
+        "python_3": py_unwrap_args,
+        "javascript": None
+    }
